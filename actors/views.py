56d4fadf4578029008697ac573bb9ccb5db073a1
lucas luna <lucas.luna@dadoteca.com.br>
2025-08-20 17:55:11 -0300

    autenticacao com a funcao permition,refresh,verify

--- actors/views.py
+++ actors/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 from actors.models import Actor
-from  actors.serializers import ActorSerializer
+from actors.serializers import ActorSerializer
 
 
 class ActorCreateListView(generics.ListCreateAPIView):
